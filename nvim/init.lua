53beb163a12757bdd58f4c20d3fb8cfce411ec1c
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2024-09-14 15:39:58 +0700

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -15,9 +15,7 @@ vim.o.softtabstop = 4
 vim.o.splitright = true
 vim.o.splitbelow = true
 vim.o.tabstop = 4
-vim.schedule(function()
-  vim.opt.clipboard = 'unnamedplus'
-end)
+vim.o.clipboard = 'unnamedplus'
 vim.o.breakindent = true
 vim.o.undofile = true
 vim.o.signcolumn = 'yes'
